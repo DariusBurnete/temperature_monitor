ce01be7365b74fb2d233805d12c7b39fd236a372
DariusBurnete <90399175+DariusBurnete@users.noreply.github.com>
2026-04-29 18:44:13 +0300

    model, DB, controller

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using temperature_system.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    var dbPath = db.Database.GetDbConnection().ConnectionString;
+    var dir = Path.GetDirectoryName(dbPath.Replace("Data Source=", ""));
+    if (!string.IsNullOrEmpty(dir))
+        Directory.CreateDirectory(dir);
+
+    db.Database.Migrate(); 
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.MapControllers();
+
+app.Run();
